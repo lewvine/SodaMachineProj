01bbb8c5f03c9eed0f83ff4ee1dc59d0f724868e
Lewis Vine <lew.vine@gmail.com>
2021-02-16 12:23:54 -0500

    first commit

---
+++ SodaMachine/Penny.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Penny:Coin
+    {
+        //Member Variables (Has A)
+
+        //Constructor (Spawner)
+        public Penny()
+        {
+            Name = "Penny";
+            value = 0.01;
+        }
+        //Member Methods (Can Do)
+    }
+}
