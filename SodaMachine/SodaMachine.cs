bd5458289edea96d061bd369cf74ddd1d9400bdc
Lewis Vine <lew.vine@gmail.com>
2021-02-16 15:00:28 -0500

    added some methods to SodaMachine

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -102,28 +102,49 @@ namespace SodaMachine
         //If it does have one, return true.  Else, false.
         private bool RegisterHasCoin(string name)
         {
-           
+            Coin coin = _register.Find(Coin => Coin.Name == name);
+            if (coin != null)
+            {
+                return true;
+            }
+            return false;
         }
         //Reusable method to return a coin from the register.
         //Returns null if no coin can be found of that name.
         private Coin GetCoinFromRegister(string name)
         {
-            
+            Coin coin = _register.Find(Coin => Coin.Name == name);
+            if (coin != null)
+            {
+                return coin;
+            }
+            return null;
         }
         //Takes in the total payment amount and the price of can to return the change amount.
         private double DetermineChange(double totalPayment, double canPrice)
         {
-            
+            double change = totalPayment - canPrice;
+            return change;
         }
-        //Takes in a list of coins to returnt he total value of the coins as a double.
+        //Takes in a list of coins to return the total value of the coins as a double.
         private double TotalCoinValue(List<Coin> payment)
         {
-           
+            double coinTotal = 0;
+
+           foreach (Coin coin in payment)
+            {
+                coinTotal += coin.Value;    
+            }
+
+            return coinTotal;
         }
         //Puts a list of coins into the soda machines register.
         private void DepositCoinsIntoRegister(List<Coin> coins)
         {
-           
+            foreach (Coin coin in coins)
+            {
+                _register.Add(coin);
+            }
         }
     }
 }
